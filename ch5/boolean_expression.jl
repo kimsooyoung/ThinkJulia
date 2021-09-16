c9b5f801e1edb4668d1fd2a11abafcf19356b54c
kimsooyoung <tge1375@naver.com>
2021-09-17 02:51:20 +0900

    Ongoing Ch5

---
+++ ch5/boolean_expression.jl
@@ -0,0 +1,11 @@
+println(5 == 5)
+
+x = 12
+y = 5
+
+println(x != y)
+println(x ≠ y)
+println(x > y)
+println(x < y)
+println(x ≥ y)
+println(x ≤ y)
---
+++ ch5/division.jl
@@ -0,0 +1,7 @@
+min = 105
+time_float = min / 60
+time_int   = min ÷ 60
+
+mod = min % 60
+
+print([min, time_float, time_int, mod])
