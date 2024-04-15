21f570fa797c60ad2ae67fa42292bb48d043e1e4
kimsooyoung <tge1375@naver.com>
2024-04-15 20:35:32 +0900

    ch6 start

---
+++ ch6/ch6_fruitful_functions.jl
@@ -0,0 +1,27 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 04e35f10-fb1c-11ee-1415-9751c1d2d99b
+md"""
+# Sooyoung Kim's Think Julia 
+
+![image](https://github.com/kimsooyoung/ThinkJulia/assets/12381733/260603e2-20ed-428e-b15b-0381f73fb46c)
+"""
+
+# ╔═╡ 9af876cf-9da8-4c96-a3a1-f218ae56d804
+md"""
+## Fruitful Functions (유익 함수)
+
+실행 결과를 반환하는 함수 즉, 유의미한 값을 return하는 함수에 대해 배워보겠습니다. 
+"""
+
+# ╔═╡ 600baf14-4487-4651-a390-94cebe38fa9f
+
+
+# ╔═╡ Cell order:
+# ╟─04e35f10-fb1c-11ee-1415-9751c1d2d99b
+# ╟─9af876cf-9da8-4c96-a3a1-f218ae56d804
+# ╠═600baf14-4487-4651-a390-94cebe38fa9f
