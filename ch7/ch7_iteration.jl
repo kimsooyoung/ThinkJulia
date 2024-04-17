547cb27ec1343d13d1713f5ddde1716e162e7f43
kimsooyoung <tge1375@naver.com>
2024-04-17 23:46:38 +0900

    ch7 start

---
+++ ch7/ch7_iteration.jl
@@ -0,0 +1,27 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e2e596e0-fcc8-11ee-2a95-2b0490ab06df
+md"""
+# Sooyoung Kim's Think Julia 
+
+![image](https://github.com/kimsooyoung/ThinkJulia/assets/12381733/260603e2-20ed-428e-b15b-0381f73fb46c)
+"""
+
+# ╔═╡ d0e7ba49-6828-4f55-b704-a30bcc4c2079
+md"""
+## Iteration (반복)
+
+이번 챕터에서는 반복 키워드인 for과 while에 대해서 배워봅니다. 이번 챕터를 마무리하면 이론적으로 여러분은 그 어떠한 문제도 프로그래밍으로 구현할 수 있습니다. [튜링 완전](https://ko.wikipedia.org/wiki/%ED%8A%9C%EB%A7%81_%EC%99%84%EC%A0%84)
+"""
+
+# ╔═╡ 9a4c7836-b726-4da4-bb51-e5c363538d0a
+
+
+# ╔═╡ Cell order:
+# ╟─e2e596e0-fcc8-11ee-2a95-2b0490ab06df
+# ╟─d0e7ba49-6828-4f55-b704-a30bcc4c2079
+# ╠═9a4c7836-b726-4da4-bb51-e5c363538d0a
