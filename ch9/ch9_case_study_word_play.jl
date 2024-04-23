e5c27f0007a3b462fe4fffbaf214da1f876eb5eb
kimsooyoung <tge1375@naver.com>
2024-04-23 19:39:49 +0900

    ch9 start

---
+++ ch9/ch9_case_study_word_play.jl
@@ -0,0 +1,22 @@
+### A Pluto.jl notebook ###
+# v0.19.40
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a193f7b0-015d-11ef-2cd8-8b90df9391a2
+md"""
+# Sooyoung Kim's Think Julia 
+
+![image](https://github.com/kimsooyoung/ThinkJulia/assets/12381733/260603e2-20ed-428e-b15b-0381f73fb46c)
+"""
+
+# ╔═╡ f6210c3d-518a-45ac-ba39-2be8095394fe
+md"""
+## Case Study: Word Play (단어로 놀기)
+
+"""
+
+# ╔═╡ Cell order:
+# ╠═a193f7b0-015d-11ef-2cd8-8b90df9391a2
+# ╠═f6210c3d-518a-45ac-ba39-2be8095394fe
