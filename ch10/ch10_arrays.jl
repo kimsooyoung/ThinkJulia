8aa195495e24320b33648a0b7bd3f3659850fdba
kimsooyoung <tge1375@naver.com>
2024-04-28 14:06:40 +0900

    ch10 ongoing

--- ch10/ch10_arrays.jl
+++ ch10/ch10_arrays.jl
@@ -109,7 +109,7 @@ md"""
 
 # ╔═╡ a01fcc4d-e0f4-4c61-84ac-066af0485ffe
 md"""
-배열 내에 존재하는 모든 원소들을 차례대로 print하고 싶은 경우 아래와 같이 for-loop을 통한 순회가 가능합니다.
+> 배열 내에 존재하는 모든 원소들을 차례대로 print하고 싶은 경우 아래와 같이 for-loop을 통한 순회가 가능합니다.
 """
 
 # ╔═╡ ad47bc12-f8b5-4e25-8929-d4b4c51c7dc8
@@ -147,7 +147,168 @@ md"""
 ## 10.4 배열 자르기(slicing) 
 """
 
+# ╔═╡ 02b261d1-2915-4717-831b-fa181f1cd8ac
+md"""
+> 대괄호 연산을 통해 배열의 일부를 참조하는 slicing 작업도 매우 자주 사용됩니다. 
+"""
+
 # ╔═╡ 98665fb3-5c55-4092-bc6e-6f75f892aa25
+begin
+	kor_abc = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ']
+	@show kor_abc[1:3]
+end
+
+# ╔═╡ eee77df0-0e3f-4ddc-9612-ae443ad45169
+md"""
+특히 배열은 가변(mutable)이기 때문에 생각 없이 코딩을 하다 보면 본래 배열값이 어느 순간 바뀌어 예상치 못한 오류가 생길 수 있지요. 
+
+때문에 기존 배열의 복사본을 만들어두곤 하며, 이 때 **[:]**를 사용하면 손쉽게 기존 배열의 복사본을 만들 수 있습니다.
+"""
+
+# ╔═╡ 863d56d3-37a6-409a-a916-98574f977ce0
+begin
+	kor_abc_copy = kor_abc[:]
+	kor_abc[1:3] = ['a', 'b', 'c']
+	println(kor_abc, kor_abc_copy)
+end
+
+# ╔═╡ a2da355e-4758-41ea-ad8f-d0cd9c0e7fc0
+md"""
+## 10.5 Built-in 배열 Library 
+"""
+
+# ╔═╡ 0d2c526e-7530-436c-ae87-1da18a09f49d
+md"""
+> Julia에는 배열을 다루는 여러 유용한 함수들이 내장되어 있습니다. 이들에 대해서 간단히 하나씩 짚고 넘어가겠습니다.
+
+* **push!(<array>, <new-element>)** : array의 맨 마지막 원소로 new element를 삽입합니다.
+"""
+
+# ╔═╡ b0acd668-bdc6-46b4-9bcb-916c907dc6b7
+begin
+	my_favo_fruits = ["banana", "melon", "orange"]
+	push!(my_favo_fruits, "mango")
+
+	print(my_favo_fruits)
+end
+
+# ╔═╡ cc8fbe70-7e04-425e-a73c-c24b14ac573c
+md"""
+* **append!(<array1>, <array2>)** : array1에 이어서 array2를 이어 붙입니다. array1은 변하지만 array2는 그대로 유지된다는 사실에 주의합니다.
+"""
+
+# ╔═╡ e095c534-e940-4bf3-9bab-3b9e6efa04bf
+begin
+	kor_last_names = ["kim", "lee", "park", "choi"]
+	kor_last_names2 = ["jeong", "kang", "cho", "hwang"]
+	append!(kor_last_names, kor_last_names2)
+
+	print(kor_last_names)
+end
+
+# ╔═╡ 0e1cd334-5ab2-4243-a40f-d33915b116a8
+md"""
+* **sort!(<array>)** : array를 정렬합니다. 문자의 경우 알파벳 순으로, 숫자의 경우 작은 크기 순으로 정렬됩니다.
+"""
+
+# ╔═╡ 5f8492b7-597f-4be3-9f0d-c60f44a1c663
+begin
+	sort!(kor_last_names)
+	
+	print(kor_last_names)
+end
+
+# ╔═╡ e0631198-06e4-4f5c-b29f-006ac5af8893
+md"""
+!!! notes
+	Julia에서는 입력 매개변수를 변형시키는 함수의 경우 함수명 뒤에 느낌표(!)를 붙이는 관행이 있습니다. sort() 함수는 정렬된 배열을 반환하는 반면, sort!()함수는 입력 자체를 변형시키지요.
+"""
+
+# ╔═╡ bb1d9ee7-18ff-4286-9fbf-a56ee268a1bc
+import Random
+
+# ╔═╡ 0546cf14-366c-4378-a7b0-820b09de924a
+let
+	rand_shuffle = Random.randperm(length(kor_last_names))
+	kor_last_names = [ name for name in kor_last_names[rand_shuffle] ]
+
+	@show sort(kor_last_names)
+	@show kor_last_names
+	
+	@show sort!(kor_last_names)
+	@show kor_last_names
+end
+
+# ╔═╡ 2e382668-889c-44da-b788-61e0951c16fc
+md"""
+## 10.6 Map, Filter, Reduce 
+"""
+
+# ╔═╡ 225800fe-f34b-4631-ba2d-78529cf399aa
+md"""
+> 배열을 다루는 대부분의 작업은 map / filter / reduce 라는 패턴을 갖습니다. 
+
+배열을 순회하면서 모든 원소에 동일한 작업을 적용하거나(map), 조건을 만족하는 원소만 추려낸다거나(filter), 원소의 모두를 대표하는 하나의 결과를 누적한다거나(reduce) 하는 것이지요.
+
+이는 함수형 프로그래밍에서 가장 처음 배우는 패턴이기도 합니다.
+
+**map example**
+"""
+
+# ╔═╡ 4604a344-ff7f-4d37-a0ff-47968bee0b23
+function capitalizeall(t)
+    res = []
+    for s in t
+        push!(res, uppercase(s))
+    end
+    res
+end
+
+# ╔═╡ ea0473de-b6d5-419b-9c49-0f5c0f63da7f
+capitalizeall(['h', 'E', 'l', 'L', 'o'])
+
+# ╔═╡ 1aa61545-bcdd-4d0e-b6ac-de9a49de98cf
+md"""
+**filter example**
+"""
+
+# ╔═╡ 0525a29c-254c-401f-a4e3-01912517eeb3
+function onlyupper(t)
+    res = []
+    for s in t
+        if s == uppercase(s)
+            push!(res, s)
+        end
+    end
+    res
+end
+
+# ╔═╡ be201db7-294b-4a9c-809e-f69bc74b26c7
+onlyupper(['h', 'E', 'l', 'L', 'o'])
+
+# ╔═╡ 3add1925-bc4f-43c8-b9b3-49cb2cb252d8
+md"""
+**reduce example**
+"""
+
+# ╔═╡ 8aba8070-eef4-4198-83c7-7a0e4e00ccaa
+function addall(t)
+    total = 0
+    for x in t
+        total += x
+    end
+    total
+end
+
+# ╔═╡ 07b2188c-c864-43bd-bc1b-c620b8f81e52
+addall([-2, -1, 0, 1, 2, 3, 4])
+
+# ╔═╡ d1073d11-8d1e-4229-9dd9-6e4b98a895f5
+md"""
+## 10.7 Dot 연산자
+"""
+
+# ╔═╡ 82d514a5-e149-4fee-b220-6865bac898f2
 
 
 # ╔═╡ 38078e41-8b2c-43e5-bf44-7cc0731ab799
@@ -157,10 +318,28 @@ md"""
 > words.txt 파일 내 단어 중에서, 길이가 20글자 이상인 단어만 출력하는 프로그램을 작성해보세요. (모든 단어는 영어로 구성되어 있습니다.)
 """
 
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+Random = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+[[Random]]
+deps = ["Serialization"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+"""
+
 # ╔═╡ Cell order:
 # ╟─552ca602-0393-11ef-3013-abde7f67d7ee
 # ╟─16efa2b8-50bb-4c14-9a61-b9197e074835
-# ╠═d31a2bdc-7ff8-4d7a-8f15-3d1b71bfee0b
+# ╟─d31a2bdc-7ff8-4d7a-8f15-3d1b71bfee0b
 # ╟─74e7bc73-f544-4e1c-a6bd-02522fc5f58c
 # ╠═62627c1a-d809-452a-939a-eb7430173c8b
 # ╟─cbdef4ad-45e8-48c5-afc9-9706a2d76b19
@@ -185,5 +364,32 @@ md"""
 # ╟─27db08df-e0b4-458e-b947-b1f9b51ff475
 # ╠═e8ae4140-b583-4f95-9862-39bc7bc6c5eb
 # ╟─c38ac06f-d11c-4f31-96f8-3fdc6f77c86a
+# ╟─02b261d1-2915-4717-831b-fa181f1cd8ac
 # ╠═98665fb3-5c55-4092-bc6e-6f75f892aa25
+# ╟─eee77df0-0e3f-4ddc-9612-ae443ad45169
+# ╠═863d56d3-37a6-409a-a916-98574f977ce0
+# ╟─a2da355e-4758-41ea-ad8f-d0cd9c0e7fc0
+# ╟─0d2c526e-7530-436c-ae87-1da18a09f49d
+# ╠═b0acd668-bdc6-46b4-9bcb-916c907dc6b7
+# ╟─cc8fbe70-7e04-425e-a73c-c24b14ac573c
+# ╠═e095c534-e940-4bf3-9bab-3b9e6efa04bf
+# ╟─0e1cd334-5ab2-4243-a40f-d33915b116a8
+# ╠═5f8492b7-597f-4be3-9f0d-c60f44a1c663
+# ╟─e0631198-06e4-4f5c-b29f-006ac5af8893
+# ╠═bb1d9ee7-18ff-4286-9fbf-a56ee268a1bc
+# ╠═0546cf14-366c-4378-a7b0-820b09de924a
+# ╟─2e382668-889c-44da-b788-61e0951c16fc
+# ╟─225800fe-f34b-4631-ba2d-78529cf399aa
+# ╠═4604a344-ff7f-4d37-a0ff-47968bee0b23
+# ╠═ea0473de-b6d5-419b-9c49-0f5c0f63da7f
+# ╟─1aa61545-bcdd-4d0e-b6ac-de9a49de98cf
+# ╠═0525a29c-254c-401f-a4e3-01912517eeb3
+# ╠═be201db7-294b-4a9c-809e-f69bc74b26c7
+# ╟─3add1925-bc4f-43c8-b9b3-49cb2cb252d8
+# ╠═8aba8070-eef4-4198-83c7-7a0e4e00ccaa
+# ╠═07b2188c-c864-43bd-bc1b-c620b8f81e52
+# ╟─d1073d11-8d1e-4229-9dd9-6e4b98a895f5
+# ╠═82d514a5-e149-4fee-b220-6865bac898f2
 # ╟─38078e41-8b2c-43e5-bf44-7cc0731ab799
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
