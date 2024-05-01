fef70e509bf48abde618063f85b81079715ba613
kimsooyoung <tge1375@naver.com>
2024-05-01 23:51:48 +0900

    ch11 start

---
+++ ch11/ch11_dictionary.jl
@@ -0,0 +1,25 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2d515d70-07ca-11ef-1060-fd2ba819c10a
+md"""
+# Sooyoung Kim's Think Julia 
+
+![image](https://github.com/kimsooyoung/ThinkJulia/assets/12381733/260603e2-20ed-428e-b15b-0381f73fb46c)
+"""
+
+# ╔═╡ 199b0ba2-d6df-42c6-9b23-785cb0b114ae
+md"""
+## Dictionary (딕셔너리)
+"""
+
+# ╔═╡ 89b1902d-21c9-4171-ba46-07bb0ea7f3cc
+
+
+# ╔═╡ Cell order:
+# ╟─2d515d70-07ca-11ef-1060-fd2ba819c10a
+# ╟─199b0ba2-d6df-42c6-9b23-785cb0b114ae
+# ╠═89b1902d-21c9-4171-ba46-07bb0ea7f3cc
